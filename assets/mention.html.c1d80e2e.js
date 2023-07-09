f2538173d29040cba9cbbce536cc4e01ba3ff9b8
HotoRas <HotoRas@users.noreply.github.com>
2023-07-09 15:44:56 +0000

    deploy: cf8ab6add22e2a1b7e6d3175b90a17a55ddf54d0

---
+++ assets/mention.html.c1d80e2e.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-e6674fc2","path":"/ko/docs/features/mention.html","title":"\uBA58\uC158","lang":"ko-KR","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1688565676000,"contributors":[{"name":"Dohyeon Jeon","email":"dohyeon@pikokr.dev","commits":1}]},"filePathRelative":"ko/docs/features/mention.md"}');export{e as data};
